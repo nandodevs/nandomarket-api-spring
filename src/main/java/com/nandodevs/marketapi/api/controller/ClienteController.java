4be41e9b4b88edd1a16301a24e33779dbbb66ee5
Sisnando Junior <nando.devs@gmail.com>
2021-08-12 19:14:04 -0300

    Configurado o Jakarta Persistence

--- src/main/java/com/nandodevs/marketapi/api/controller/ClienteController.java
+++ src/main/java/com/nandodevs/marketapi/api/controller/ClienteController.java
@@ -1,30 +1,24 @@
 package com.nandodevs.marketapi.api.controller;
 
-import java.util.Arrays;
 import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nandodevs.marketapi.domain.model.Cliente;
 
+
 @RestController
 public class ClienteController {
 	
+	@PersistenceContext
+	private EntityManager manager;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		var cliente1 = new Cliente();
-		cliente1.setId(1L);
-		cliente1.setNome("Jubileu");
-		cliente1.setEmail("jubileu@nandoapi.com");
-		cliente1.setTelefone("98 98447-5522");
-		
-		var cliente2 = new Cliente();
-		cliente2.setId(2L);
-		cliente2.setNome("Rosa Maria");
-		cliente2.setEmail("rosamaria@nandoapi.com");
-		cliente2.setTelefone("98 99991-0123");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return manager.createQuery("from Cliente", Cliente.class).getResultList();
 	}
 }
--- src/main/java/com/nandodevs/marketapi/domain/model/Cliente.java
+++ src/main/java/com/nandodevs/marketapi/domain/model/Cliente.java
@@ -1,12 +1,26 @@
 package com.nandodevs.marketapi.domain.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(onlyExplicitlyIncluded=true)
 @Getter
 @Setter
+@Entity
 public class Cliente {
 	
+	@EqualsAndHashCode.Include
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	/*
+	 * //Usa a forma nativa do banco de dados
+	 */	
 	private Long id;
 	private String nome;
 	private String email;
